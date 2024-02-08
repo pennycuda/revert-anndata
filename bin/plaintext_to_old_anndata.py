49a37641c6ad0e9d50ec45ed8862b9c6a9c9def8
pennycuda <pcuda@andrew.cmu.edu>
2024-02-08 12:19:49 -0500

    Do not transpose

--- bin/plaintext_to_old_anndata.py
+++ bin/plaintext_to_old_anndata.py
@@ -8,7 +8,7 @@ import scipy.sparse
 
 
 def convert_raw(mtx_file: Path , obs_file: Path, var_file: Path):
-    mtx = scipy.io.mmread(mtx_file).T.tocsr()
+    mtx = scipy.io.mmread(mtx_file).tocsr()
     obs = pd.read_csv(obs_file)
     var = pd.read_csv(var_file)
     adata = anndata.AnnData(X=mtx, obs=obs, var=var)
