23636bd506f4d030cd062f8698b49a93eb551d5d
pennycuda <pcuda@andrew.cmu.edu>
2024-02-08 11:44:43 -0500

    Fix paths

--- bin/new_anndata_to_plaintext.py
+++ bin/new_anndata_to_plaintext.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import anndata
 import pandas as pd
 import numpy as np
-import scipy.sparse
+import scipy.io
 
 
 def convert(orig_file: Path):
@@ -13,14 +13,14 @@ def convert(orig_file: Path):
     # get the matrix
     mtx = adata.X
     mtx_path = Path(orig_file.name).with_suffix('.mtx')
-    scipy.sparse.save_npz(mtx_path, mtx)
+    scipy.io.mmwrite(mtx_path, mtx)
     # get the obs
     obs_data = adata.obs
-    obs_path = Path(orig_file.name, 'obs').with_suffix('.csv')
+    obs_path = Path(f'{orig_file.name}_obs').with_suffix('.csv')
     obs_data.to_csv(obs_path)
     # get the var
     var_data = adata.var
-    var_path = Path(orig_file.name, 'var').with_suffix('.csv')
+    var_path = Path(f'{orig_file.name}_var').with_suffix('.csv')
     var_data.to_csv(var_path)
 
 
