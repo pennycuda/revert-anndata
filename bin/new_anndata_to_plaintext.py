e5c8746eb31385f934356df3e4f060241015655c
pennycuda <pcuda@andrew.cmu.edu>
2024-02-08 13:43:32 -0500

    Add logging

--- bin/new_anndata_to_plaintext.py
+++ bin/new_anndata_to_plaintext.py
@@ -30,14 +30,17 @@ def convert_sa(sa_file: Path):
     adata = anndata.read_h5ad(str(sa_file))
     # get the matrix
     mtx = adata.X
+    print("Mtx shape", mtx.shape)
     mtx_path = Path('sa_mtx.mtx')
     scipy.io.mmwrite(mtx_path, mtx)
     # get the obs
     obs_data = adata.obs
+    print("Obs shape: ", obs_data.shape)
     obs_path = Path('sa_obs.csv')
     obs_data.to_csv(obs_path)
     # get the var
     var_data = adata.var
+    print("Var shape: ", var_data.shape)
     var_path = Path('sa_var.csv')
     var_data.to_csv(var_path)
 
@@ -47,6 +50,7 @@ if __name__ == '__main__':
     p.add_argument('raw_file', type=Path)
     p.add_argument('secondary_analysis_file', type=Path)
     args = p.parse_args()
-
+    print("Converting raw file")
     convert_raw(args.raw_file)
+    print("Converting secondary analysis file")
     convert_sa(args.secondary_analysis_file)
